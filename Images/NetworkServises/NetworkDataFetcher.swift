928cceef6ba8e6fe5bdff1a412860ac630de931e
AliceRomanova <aliceromanova2004@mail.ru>
2022-06-04 01:08:14 +0300

    Реализовала класс NetworkDataFetcher

--- Images/NetworkServises/NetworkDataFetcher.swift
+++ Images/NetworkServises/NetworkDataFetcher.swift
@@ -7,4 +7,39 @@
 
 import Foundation
 
-// Реализация парсинга приходящих данных 
+// Реализация парсинга приходящих данных
+
+class NetworkDataFetcher {
+    
+    
+    var networkService = NetworkService()
+    
+    
+    func fetchImages(request: String, completion: @escaping (SearchResults?) -> Void) {
+        
+        networkService.getRequest(request: request) { data, error in
+            if let error = error {
+                print("error received requesting data: \(error.localizedDescription)")
+                completion(nil)
+            }
+            
+            let decode = self.decodeJSON(type: SearchResults.self, data: data)
+            completion(decode)
+        }
+    }
+    
+    
+    func decodeJSON<T: Decodable>(type: T.Type, data: Data?) -> T? {
+        let decoder = JSONDecoder()
+        
+        guard let data = data else { return nil }
+        
+        do {
+            let objects = try decoder.decode(type.self, from: data)
+            return objects
+        } catch let jsonError {
+            print("Failed to decode JSON", jsonError)
+            return nil
+        }
+    }
+}
